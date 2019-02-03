891dcf086ce74b9f423b304c62e3a941b69c4739
Yurok86RUS <yurok.kolesnikov@gmail.com>
2019-02-03 17:13:19 +0500

    finish task

--- HW_5 20190202/src/ru/geekbrains/lesson5/Race.java
+++ HW_5 20190202/src/ru/geekbrains/lesson5/Race.java
@@ -4,14 +4,19 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Race {
+
+    boolean winner = false;
+
     private ArrayList<Stage> stages;
-
-    public ArrayList<Stage> getStages() {
-        return stages;
-    }
-
+    public ArrayList<Stage> getStages() { return stages; }
     public Race(Stage... stages) {
         this.stages = new ArrayList<>(Arrays.asList(stages));
     }
 
+    public boolean hasWinner() {
+        return winner;
+    }
+    public void setWinner() {
+        winner = true;
+    }
 }
